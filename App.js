1b45d543380886085349448afbda996e5edd9edc
PAWink <mysisterwinkwink@gmail.com>
2019-09-23 18:57:44 +0700

    version2

--- App.js
+++ App.js
@@ -16,50 +16,51 @@ const prepareStateFromWord = (given_word) => {
     completed: false
   }
 }
-
-
+ 
 class App extends React.Component {
-
+ 
   state = prepareStateFromWord(message);
-
-
-  activationHandler = (value) => {
-    this.activationHandler = (c) => {
-      let guess = [...this.state.guess, c]
-      this.setState({guess})
-      if(guess.length == this.state.chars.length){
-      if(guess.join('').toString() == this.state.word){
-      this.setState({guess: [], completed: true})
-      }else{
-      this.setState({guess: [], attempt: this.state.attempt + 1})
+ 
+  activationHandler = (c) => {
+    let guess = [...this.state.guess, c]
+    this.setState({ guess })
+    if (guess.length == this.state.chars.length) {
+      if (guess.join('').toString() == this.state.word) {
+        this.setState({ guess: [], completed: true })
+      } else {
+        this.setState({ guess: [], attempt: this.state.attempt + 1 })
       }
-      }
-     }
-     
-    this.setState({
-      guess: [...this.state.guess, value]
-    })
+    }
   }
-
   render() {
     return (
       <div>
         {
           Array.from(this.state.chars).map((item, index) => (
-            <Charactercard
+            <CharacterCard
               value={item}
               key={index}
               activationHandler={this.activationHandler}
             />
           ))
         }
-        <div>Attempt {this.state.attempt}</div>
+        <h2>Selected</h2>
         {
-          this.state.completed && <h4> completed</h4>
+          Array.from(this.state.guess).map((item, index) => (
+            <CharacterCard
+              value={item}
+              key={index}
+              activationHandler={this.activationHandler}
+            />
+          ))
+        }
+        <div>Attemp {this.state.attempt}</div>
+        {
+          this.state.completed && <h4>Complete</h4>
         }
       </div>
     )
   }
 }
-
+ 
 export default App;
