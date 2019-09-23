679331818e3ee9d7179a9ec096bf7c896543fcc7
PAWink <mysisterwinkwink@gmail.com>
2019-09-23 19:55:45 +0700

    version10

--- App.js
+++ App.js
@@ -32,6 +32,15 @@ class App extends React.Component {
       }
     }
   }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <div>This is Supaporn Pedkong</div>
+      </div>
+    )
+  }
   render() {
     return (
       <div>
